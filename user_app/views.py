79fb53215c444fb251d74c51a9bb744237ed687b
lettuce.lu <lettuce.lu@lettucelu.com>
2023-05-25 12:21:20 +0800

    後台準備接收數據

--- user_app/views.py
+++ user_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import request
+from django import http
 # Create your views here.
 
 def register(request):
@@ -11,8 +12,17 @@ def register(request):
         print(request.method)
         print(request.body)
         # username, email, password, confirmPassword
-        username = request.POST.post('username', None)
-        print(f'username = {username}')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirmPassword = request.POST.get('confirmPassword')
+        email = request.POST.get('email')
+        if not all([username, email, password, confirmPassword]):
+            # 進行進一步的判斷, 沒有異常才存入數據庫
+            print('有數據缺少')
+            return http.HttpResponseForbidden('請確實填寫各項表單數據')
+        else:
+            # 返回缺少數據給使用者
+            print('數據正常')
         return redirect('/users/login/')
 
 def login(request):
